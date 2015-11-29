5ee4806849d831ff40b552cb56e7e2245c59ef83
Chathuranga Wijesingha <cha17338@hotmail.com>
2015-11-29 22:57:51 +0530

    Small fixes

--- app/src/main/java/com/findmyhotel/MainActivity.java
+++ app/src/main/java/com/findmyhotel/MainActivity.java
@@ -13,28 +13,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
+import com.findmyhotel.model.Hotel;
+import com.findmyhotel.model.Hotels;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-    static final LatLng KIEL = new LatLng(53.551, 9.993);
     private GoogleMap map;
+    List<Marker> markers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        requestWindowFeature(Window.FEATURE_PROGRESS);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -57,20 +74,53 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-
+        markers = new ArrayList<Marker>();
         map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
                 .getMap();
 
-        if (map!=null){
-            Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-                    .title("Hamburg"));
-            Marker kiel = map.addMarker(new MarkerOptions()
-                    .position(KIEL)
-                    .title("Kiel")
-                    .snippet("Kiel is cool")
-                    .icon(BitmapDescriptorFactory
-                            .fromResource(R.mipmap.ic_launcher)));
-        }
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://findmyhotel.local")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        // prepare call in Retrofit 2.0
+        ServerAPI hotelsAPI = retrofit.create(ServerAPI.class);
+
+        Call<Hotels> call = hotelsAPI.loadHotels();
+        //asynchronous call
+        call.enqueue(new Callback<Hotels>() {
+            @Override
+            public void onResponse(Response<Hotels> response, Retrofit retrofit) {
+                setProgressBarIndeterminateVisibility(false);
+
+                if (map != null) {
+                    for (Hotel item : response.body().items) {
+                        Marker marker = map.addMarker(new MarkerOptions()
+                                        .position(new LatLng(item.getLatitude(), item.getLongitude()))
+                                        .title(item.getName())
+                                .snippet(item.getAddress())
+                        );
+                        markers.add(marker);
+                    }
+
+                    LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                    for (Marker marker : markers) {
+                        builder.include(marker.getPosition());
+                    }
+                    LatLngBounds bounds = builder.build();
+
+                    int padding = 0; // offset from edges of the map in pixels
+                    CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+                    map.animateCamera(cu);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Toast.makeText(MainActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
